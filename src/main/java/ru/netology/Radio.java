1183c98501e5e9dfe3746540425607f158625885
gulyolik <olia.guliaeva@yandex.ru>
2022-12-04 15:10:46 +0300

    Fixes

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,26 +1,29 @@
 package ru.netology;
+
 public class Radio {
     private int currentStationNumber;
     private int currentVolumeLevel;
-    public int getCurrentStationNumber(){
+
+    public int getCurrentStationNumber() {
 
         return currentStationNumber;
     }
-    public void setCurrentStationNumber(int currentStationNumber){
-        if(currentStationNumber<0){
+
+    public void setCurrentStationNumber(int currentStationNumber) {
+        if (currentStationNumber < 0) {
             return;
         }
-        if(currentStationNumber>9){
+        if (currentStationNumber > 9) {
             return;
         }
         this.currentStationNumber = currentStationNumber;
-        }
+    }
 
     public void setCurrentVolumeLevel(int currentVolumeLevel) {
-        if(currentVolumeLevel<0){
+        if (currentVolumeLevel < 0) {
             return;
         }
-        if(currentVolumeLevel>10){
+        if (currentVolumeLevel > 10) {
             return;
         }
         this.currentVolumeLevel = currentVolumeLevel;
@@ -34,34 +37,33 @@ public class Radio {
     public void nextStationNumber() {
         int next = currentStationNumber + 1;
         currentStationNumber = next;
-        if (currentStationNumber>9) {
+        if (currentStationNumber > 9) {
             currentStationNumber = 0;
         }
 
     }
-        public void prevStationNumber(){
-            int prev = currentStationNumber - 1;
-            currentStationNumber = prev;
-            if(currentStationNumber<0){
-                currentStationNumber = 9;
-            }
-        }
 
-    public void increaseVolume(){
-        if(currentVolumeLevel<10){
-            currentVolumeLevel = currentVolumeLevel+1;
+    public void prevStationNumber() {
+        int prev = currentStationNumber - 1;
+        currentStationNumber = prev;
+        if (currentStationNumber < 0) {
+            currentStationNumber = 9;
         }
-        else{
+    }
+
+    public void increaseVolume() {
+        if (currentVolumeLevel < 10) {
+            currentVolumeLevel = currentVolumeLevel + 1;
+        } else {
             currentVolumeLevel = 0;
         }
 
     }
 
-    public void decreaseVolume(){
-        if(currentVolumeLevel>0){
-            currentVolumeLevel = currentVolumeLevel-1;
-        }
-        else {
+    public void decreaseVolume() {
+        if (currentVolumeLevel > 0) {
+            currentVolumeLevel = currentVolumeLevel - 1;
+        } else {
             currentVolumeLevel = 10;
         }
     }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -6,44 +6,46 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void shouldSetIntoTheBorders(){
+    public void shouldSetIntoTheBorders() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(-1);
         int expected = 0;
         int actual = radio.getCurrentStationNumber();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSetCurrentStationNumberIntoTheBorder(){
+    public void shouldSetCurrentStationNumberIntoTheBorder() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(15);
         int expected = 0;
         int actual = radio.getCurrentStationNumber();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void shouldSkipToTheNextStation(){
+    public void shouldSkipToTheNextStation() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(9);
         radio.nextStationNumber();
         int expected = 0;
         int actual = radio.getCurrentStationNumber();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
-    public void shouldSkipToTheNextStations(){
+    public void shouldSkipToTheNextStations() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(8);
         radio.nextStationNumber();
         int expected = 9;
         int actual = radio.getCurrentStationNumber();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSkipToThePreviousStation(){
+    public void shouldSkipToThePreviousStation() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(0);
         radio.prevStationNumber();
@@ -51,17 +53,19 @@ public class RadioTest {
         int actual = radio.getCurrentStationNumber();
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void shouldSkipToThePrevStation(){
+    public void shouldSkipToThePrevStation() {
         Radio radio = new Radio();
         radio.setCurrentStationNumber(1);
         radio.prevStationNumber();
         int expected = 0;
         int actual = radio.getCurrentVolumeLevel();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void shouldSetCurrentVolumeLevelIntoBorders(){
+    public void shouldSetCurrentVolumeLevelIntoBorders() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(-1);
         int expected = 0;
@@ -70,26 +74,26 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetCurrentVolumeLevelIntoBoarder(){
+    public void shouldSetCurrentVolumeLevelIntoBoarder() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(15);
         int expected = 0;
         int actual = radio.getCurrentVolumeLevel();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldIncreaseVolume(){
+    public void shouldIncreaseVolume() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(5);
         radio.increaseVolume();
         int expected = 6;
         int actual = radio.getCurrentVolumeLevel();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSkipToTheNextVolumeLevel(){
+    public void shouldSkipToTheNextVolumeLevel() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(10);
         radio.increaseVolume();
@@ -99,21 +103,22 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSkipToThePreviousVolumeLevel(){
+    public void shouldSkipToThePreviousVolumeLevel() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(0);
         radio.decreaseVolume();
         int expected = 10;
         int actual = radio.getCurrentVolumeLevel();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void shouldSkipToThePrevVolLevel(){
+    public void shouldSkipToThePrevVolLevel() {
         Radio radio = new Radio();
         radio.setCurrentVolumeLevel(7);
         radio.decreaseVolume();
         int expected = 6;
         int actual = radio.getCurrentVolumeLevel();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
